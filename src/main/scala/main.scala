23be685345be9a5449937bb7a98880f50562d2da
Jaca313 <56292603+Jaca313@users.noreply.github.com>
2024-03-13 01:53:27 +0100

    feat: Added Frame with drawn cell

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,4 +1,25 @@
+import cats.effect.unsafe.implicits.global
+import doodle.core.*
+import doodle.image.*
+import doodle.image.syntax.all.*
+import doodle.java2d.*
+import doodle.syntax.all.*
+
+
+val screenHeight = 800
+val screenWidth = 800
+val resolution = 40
+
+val cell =
+  Picture
+    .square(40)
+    .strokeColor(Color.black)
+    .strokeWidth(1.0)
+
+
 @main
 def main(): Unit = {
-  println("Hello world!")
+  val frame = Frame.default.withSize(screenHeight, screenWidth).withBackground(Color.whiteSmoke)
+  cell.drawWithFrame(frame)
+
 }
