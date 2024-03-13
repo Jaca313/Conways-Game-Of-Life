46782b5eea6585d4cb16abd10334b39a7a692ab6
Jaca313 <56292603+Jaca313@users.noreply.github.com>
2024-03-13 21:41:01 +0100

    feat: Implemented Conway Animation Object. It is now possible to run animation with random filled grid and write it to gif file.

---
+++ src/main/scala/ConwayAnimation.scala
@@ -0,0 +1,115 @@
+import cats.effect.IO
+import cats.effect.unsafe.implicits.global
+import cats.instances.all.*
+import doodle.core.*
+import doodle.core.format.Gif
+import doodle.effect.*
+import doodle.image.*
+import doodle.image.syntax.all.*
+import doodle.interact.syntax.all.*
+import doodle.java2d.{Picture, *}
+import doodle.java2d.effect.*
+import doodle.syntax.all.*
+import fs2.Stream
+
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.*
+import scala.util.Random
+
+object ConwayAnimation {
+  import cats.effect.IO
+  import cats.effect.unsafe.implicits.global
+  import cats.instances.all.*
+  import doodle.core.*
+  import doodle.core.format.Gif
+  import doodle.interact.syntax.all.*
+  import doodle.java2d.effect.*
+  import doodle.syntax.all.*
+  import fs2.Stream
+
+  import scala.concurrent.duration.*
+
+  val screenHeight = 1200
+  val screenWidth = 1200
+  val resolution = 30
+
+  val cellCountX: Int = screenHeight / resolution
+  val cellCountY: Int = screenWidth / resolution
+
+  val filledPercentage: Int = 70 //How much of Black vs White in random Grid (0-100)
+  val cell =
+    Picture
+      .square(resolution)
+      .strokeWidth(3)
+      .strokeColor(Color.grey)
+
+  val frame =
+    Frame.default.withTitle("Conways Game of Life Doodle").withSize(screenWidth, screenHeight).withBackground(Color.white)
+
+  var currentGrid: Array[Array[Int]] = randomizeGrid()
+
+  def randomizeGrid(): Array[Array[Int]] =
+    //Create Array
+    val grid1: Array[Array[Int]] = Array.ofDim[Int](cellCountX, cellCountY)
+    //Fill with random Black and White
+    for (i <- 0 until cellCountX; j <- 0 until cellCountY) {
+      grid1(i)(j) = if(Random.between(0, 100) > filledPercentage) 1 else 0
+    }
+    grid1
+
+  def calculateNeighbor(x:Int,y:Int): Int = {
+    var sum = 0
+    for (i <- -1 until 2; j <- -1 until 2) {
+      val row = (x + i + cellCountX)  % cellCountX
+      val col = (y + j + cellCountY) % cellCountY
+      sum += currentGrid(row)(col)
+    }
+    sum -= currentGrid(x)(y)
+    sum
+  }
+
+  def calculateNewGrid(): Array[Array[Int]] ={
+    val newGrid: Array[Array[Int]] = Array.ofDim[Int](cellCountX, cellCountY)
+    for (i <- 0 until cellCountX; j <- 0 until cellCountY) {
+      newGrid(i)(j) =
+        val neighborCount = calculateNeighbor(i,j)
+        if(currentGrid(i)(j) == 1 && (neighborCount < 2 ||  neighborCount > 3)) 0
+        else if(currentGrid(i)(j) == 0 && neighborCount == 3 || currentGrid(i)(j) == 1 && (neighborCount == 2 || neighborCount == 3)) 1
+        else 0
+    }
+    newGrid
+  }
+
+  def grid(): Picture[Unit] =
+    currentGrid = calculateNewGrid()
+
+    val origin = Picture.circle(1).at(screenWidth, screenHeight)
+    var vertices = origin
+    for (i <- 0 until cellCountX; j <- 0 until cellCountY) {
+      vertices = cell
+        .fillColor(if (currentGrid(i)(j) == 1) Color.black else Color.white)
+        .at((i + 0.5) * resolution, (j + 0.5) * resolution)
+        .on(vertices)
+    }
+    vertices
+
+
+  val animation: Stream[IO, Picture[Unit]] =
+    Stream(1).repeat
+      .debounce[IO](50.millis)
+      .scan((1, 0)) { (state, _) =>
+        val (inc, size) = state
+        (1,1)
+      }
+      .map { case (_, s) => grid() }
+
+  def go() =
+    animation.animateFrames(frame)
+
+  def write() =
+    animation.take(20).write[Gif]("conway.gif", frame)
+
+
+
+
+}
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,19 +1,24 @@
+import cats.effect.IO
 import cats.effect.unsafe.implicits.global
 import doodle.core.*
+import doodle.core.format.*
 import doodle.image.*
 import doodle.image.syntax.all.*
 import doodle.java2d.*
 import doodle.syntax.all.*
+import doodle.effect.*
+import fs2.Stream
 
+import java.util.concurrent.TimeUnit
 import scala.util.Random
-
+import concurrent.duration.DurationInt
 
 val screenHeight = 800
 val screenWidth = 800
 val resolution = 40
 
-val cellCountX = screenHeight / resolution
-val cellCountY = screenWidth / resolution
+val cellCountX: Int = screenHeight / resolution
+val cellCountY: Int = screenWidth / resolution
 
 val cell =
   Picture
@@ -22,10 +27,23 @@ val cell =
     .strokeWidth(1.0)
 
 
+//def explorer(using
+//             intGui: ExploreInt[Component],
+//             colorGui: ExploreColor[Component],
+//             layoutGui: Layout[Component],
+//            ) = {
+//  import intGui._
+//  import colorGui._
+//
+//  int("Base Size").within(1 to 30)
+//    .beside(int("Iterations").within(1 to 6).withDefault(2))
+//    .above(color("Stroke Color"))
+//}
+
 @main
 def main(): Unit = {
   //Create Frame
-  val frame = Frame.default.withSize(screenHeight, screenWidth).withBackground(Color.whiteSmoke)
+  var frame = Frame.default.withTitle("Conways Game of Life Doodle").withSize(screenHeight, screenWidth).withBackground(Color.whiteSmoke).withClearToColor(Color.whiteSmoke) //.withCenterAtOrigin
   val canvas = frame.canvas()
 
   val origin = Picture.circle(1).at(screenWidth, screenHeight)
@@ -38,15 +56,35 @@ def main(): Unit = {
     grid1(i)(j) = Random.between(0, 2)
   }
 
-  var vertices = origin
+  //Fill grid1 with Random Noise 0-1
+  for (i <- 0 until 20; j <- 0 until 20) {
+    grid2(i)(j) = Random.between(0, 2)
+  }
+
+
+
   //Draw Grid1
+  var vertices = origin
   for (i <- 0 until cellCountX; j <- 0 until cellCountY) {
     vertices = cell
       .fillColor(if (grid1(i)(j) == 1) Color.black else Color.white)
+      .strokeWidth(3).strokeColor(Color.grey)
       .at((i + 0.5) * resolution, (j + 0.5) * resolution)
       .on(vertices)
   }
+  //vertices.drawWithFrame(frame)
 
-  vertices.drawWithFrame(frame)
+  //Draw Grid2
+  var vertices2 = origin
+  for (i <- 0 until cellCountX; j <- 0 until cellCountY) {
+    vertices2 = cell
+      .fillColor(if (grid1(i)(j) == 1) Color.black else Color.white)
+      .strokeWidth(3).strokeColor(Color.grey)
+      .at((i + 0.5) * resolution, (j + 0.5) * resolution)
+      .on(vertices2)
+  }
+    ConwayAnimation.write()
+    ConwayAnimation.go()
 
 }
+
