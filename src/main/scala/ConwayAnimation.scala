32a22dee19b4366435bad13422e07f8ddd9821a1
Jaca313 <56292603+Jaca313@users.noreply.github.com>
2024-03-15 02:33:13 +0100

    feat: Change ConwayAnimation object to case class. Will work now with builder style immutable function chaining. Hidden helper functions as private.

--- src/main/scala/ConwayAnimation.scala
+++ src/main/scala/ConwayAnimation.scala
@@ -3,7 +3,7 @@ import cats.effect.unsafe.implicits.global
 import doodle.core.*
 import doodle.core.format.Gif
 import doodle.interact.syntax.all.*
-import doodle.java2d.effect.GifEncoder
+import doodle.java2d.effect.{Center, GifEncoder, Redraw, Size}
 import doodle.java2d.{Picture, *}
 import doodle.syntax.all.*
 import fs2.Stream
@@ -11,46 +11,83 @@ import fs2.Stream
 import scala.concurrent.duration.*
 import scala.io.Source
 import scala.util.Random
-
 import java.util.concurrent.TimeUnit
 
 object ConwayAnimation {
+  val default: ConwayAnimation =
+    ConwayAnimation(
+      screenHeight = 800,
+      screenWidth = 800,
+      cellCountX = 10,
+      cellCountY = 10,
+      resolution = 80,
+      filledPercentage = 70,
+      frameTime = 250,
+      fileFrameCount = 20
+    )
+}
 
-  val screenHeight = 800 //Size of Window
-  val screenWidth = 800 //Size of Window
-  var cellCountX: Int = 10 //Cell Count on X axis
-  var cellCountY: Int = 10 //Cell Count on Y axis
-  var resolution: Int = calculateResolution() //Size of Cell
-  val filledPercentage: Int = 70 //How much of Black vs White in random Grid (0-100)
-  val frameTime: Long = 250 //Time between simulation/animation frames
-  val fileFrameCount: Int = 20 //How many frames of animation to write to gif
+final case class ConwayAnimation(
+    private val screenWidth: Int,       /**Size of Window*/
+    private val screenHeight: Int,      /**Size of Window*/
+    private var cellCountX: Int,        /**Cell Count on X axis*/
+    private var cellCountY: Int,        /**Cell Count on Y axis*/
+    private var resolution: Int,        /**Size of Cell*/
+    private val filledPercentage: Int,  /**How much of Black vs White in random Grid (0-100)*/
+    private val frameTime: Long,        /**Time between simulation/animation frames*/
+    private val fileFrameCount: Int,    /**How many frames of animation to write to gif*/
+) {
+
+  /**Size the window*/
+  def withScreenSize(screenWidth: Int,screenHeight: Int): ConwayAnimation =
+    val cp = this.copy(screenWidth = screenWidth,screenHeight = screenHeight)
+    cp.copy(resolution = cp.calculateResolution())
+
+  /**Use an X by Y grid of cells when not loading from a file*/
+  def withCellDimensions(cellCountX: Int,cellCountY: Int): ConwayAnimation =
+    val cp = this.copy(cellCountX = cellCountX,cellCountY = cellCountY)
+    cp.copy(resolution = cp.calculateResolution())
+
+  /**Fill with this percentage when filling grid randomly*/
+  def withFilledCellsPercentage(filledPercentage : Int): ConwayAnimation =
+    this.copy(filledPercentage = filledPercentage)
+
+  /**Use this time between animation frames*/
+  def withAnimationFrameTime(frameTime: Long): ConwayAnimation =
+    this.copy(frameTime = frameTime)
+
+  /**Write this many frames to gif when output to file*/
+  def withWriteToGifFrameCount(fileFrameCount: Int): ConwayAnimation =
+    this.copy(fileFrameCount = fileFrameCount)
+
+
 
   //Cell Picture
-  def cell(size: Int): Picture[Unit] =
+  private def cell(size: Int): Picture[Unit] =
     Picture
       .square(size)
       .strokeWidth(3)
       .strokeColor(Color.grey)
 
   //Fill Starting Grid Randomly
-//  var currentGrid: Array[Array[Int]] = randomGrid()
-  var currentGrid: Array[Array[Int]] = loadGrid("conway.txt")
+  var currentGrid: Array[Array[Int]] = randomGrid()
+//  private var currentGrid: Array[Array[Int]] = loadGrid("conway.txt")
 
   //Create Frame(Canvas/Window) to write to
-  val frame: Frame  = Frame.default
+  private val frame: Frame  = Frame.default
     .withTitle("Conways Game of Life Doodle")
     .withSize(screenWidth, screenHeight)
     .withBackground(Color.white)
 
   //Calculate Cell Resolution to match screen size and cell count
-  def calculateResolution(): Int = {
+  private def calculateResolution(): Int = {
     val resX = screenWidth / cellCountX
     val resY = screenHeight / cellCountY
     if(resX > resY) resY else resX
   }
 
   //Generated Grid filled randomly with percentage $filledPercentage
-  def randomGrid(): Array[Array[Int]] ={
+  private def randomGrid(): Array[Array[Int]] ={
     //Create Grid Array
     val newGrid: Array[Array[Int]] = Array.ofDim[Int](cellCountX, cellCountY)
     //Fill with random Black-1 and White-0 by percentage Black/White
@@ -60,7 +97,7 @@ object ConwayAnimation {
     newGrid
   }
 
-  def loadGrid(filename: String): Array[Array[Int]] = {
+  private def loadGrid(filename: String): Array[Array[Int]] = {
     val bufferedSource = Source.fromFile(filename)
     val input: Array[String] = bufferedSource.getLines.toArray
     bufferedSource.close
@@ -85,7 +122,7 @@ object ConwayAnimation {
 
   //Calculates number of neighbors of a cell
   //Using wraparound
-  def calculateNeighbor(x:Int,y:Int): Int = {
+  private def calculateNeighbor(x:Int,y:Int): Int = {
     var sum = 0
     for (i <- -1 until 2; j <- -1 until 2) {
       val row = (x + i + cellCountX)  % cellCountX
@@ -98,7 +135,7 @@ object ConwayAnimation {
 
   //Calculate new grid based on $currentGrid according to rules at
   //https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
-  def calculateNewGrid(): Array[Array[Int]] ={
+  private def calculateNewGrid(): Array[Array[Int]] ={
     val newGrid: Array[Array[Int]] = Array.ofDim[Int](cellCountX, cellCountY)
     for (i <- 0 until cellCountX; j <- 0 until cellCountY) {
       newGrid(i)(j) =
@@ -111,7 +148,7 @@ object ConwayAnimation {
   }
 
   //Update,draw and output a picture of the state of the grid
-  def getGridPictureAndUpdateGrid(): Picture[Unit] = {
+  private def getGridPictureAndUpdateGrid(): Picture[Unit] = {
     currentGrid = calculateNewGrid()
     val origin = Picture.circle(1).at(screenWidth, screenHeight)
     var vertices = origin
@@ -124,7 +161,7 @@ object ConwayAnimation {
     vertices
   }
 
-  val animation: Stream[IO, Picture[Unit]] = {
+  private val animation: Stream[IO, Picture[Unit]] = {
     Stream(1).repeat
       .debounce[IO](Duration(frameTime, TimeUnit.MILLISECONDS))
       .map { s => getGridPictureAndUpdateGrid() }
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,6 +1,10 @@
 @main
 def main(): Unit = {
-    ConwayAnimation.write()
-    ConwayAnimation.go()
+    val conway = ConwayAnimation.default
+      .withScreenSize(1200,1200)
+      .withCellDimensions(20,20)
+    
+    conway.write()
+    conway.go()
 }
 
